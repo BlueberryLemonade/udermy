ceb4c3e4692d8ed2bf7d0b237a8bef5cc4bbfa5d
Justin <Justin.Swatloski@gmail.com>
2022-10-24 21:48:53 -0500

    placeholder slash test commit

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem () {
-    return <h2>Expense Item</h2>
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Mar 23, 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>@22</div>
+            </div>
+        </div>
+    );
 };
 
 export default ExpenseItem;
