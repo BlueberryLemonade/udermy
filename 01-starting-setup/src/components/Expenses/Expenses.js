1d1edfc8f7b4e24c5b962b80078c192742b90784
Justin <Justin.Swatloski@gmail.com>
2022-10-25 11:48:22 -0500

    end of module

---
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,22 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+import Card from "./UI/Card";
+
+function Expenses(props) {
+
+  const expenses = props.expenses;
+
+  return (
+  <Card className="expenses">
+    {expenses.map((expense) => (
+      <ExpenseItem 
+       title={expense.title}
+       amount={expense.amount} 
+       date={expense.date}
+       />
+    ))}
+  </Card>
+  )
+}
+
+export default Expenses;
