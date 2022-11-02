8b28c6f29f53774850cc1b5b9f085f2ee097a340
Justin <Justin.Swatloski@gmail.com>
2022-11-01 21:58:25 -0500

    fixed css, years properly aligned to the left.

--- 01-starting-setup/src/components/Expenses/ExpenseFilter.js
+++ 01-starting-setup/src/components/Expenses/ExpenseFilter.js
@@ -1,22 +1,23 @@
-import Years from "./Years";
 import './ExpenseFilter.css';
 
-const ExpenseFilter = (props) => {
+const ExpensesFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
 
-    const optionHandler = (selection) => {
-        props.yearSelection(selection.target.value);
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
-    return (
-        <div className="expenses-filter">
-            <div classname="expenses-filter__control">
-                <label>Select a year: </label>
-                <select  name='year' id='year' value={props.selected} onChange={optionHandler} >
-                    <Years />
-                </select>
-            </div>
-        </div>
-    );
-};
-
-export default ExpenseFilter;
+export default ExpensesFilter;
